4e811a998cf69c7f79253057b7f3893b84d010a2
Dawid Wolski <dawidspore@wp.pl>
2022-07-07 15:50:55 +0200

    Dodano druga strone, nie do konca dzialajacy skrypt w axios'ie

--- api.js
+++ api.js
@@ -1,7 +1,31 @@
 // GET
 import axios from 'axios';
+
+let mainDiv = document.querySelector('#main')
+
 export function getPosts() {
-    axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    .then((res) => console.log(res))
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => buildDivs(res.data))
     .catch((err) => console.log(err)) 
 }
+    
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";   
+}
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+}
+const buildDivs = (rows) => {
+    for(let i=0; i < rows.length; i++) {
+        let pDiv = document.createElement('p')
+        pDiv.setAttribute('id', 'apiP')
+        pDiv.innerText = rows[i].body + rows[i].id + rows[i].title + rows[i].userId
+        mainDiv.appendChild(pDiv)
+    }
+}
+
+
+sideBarOpen.addEventListener('click', openNav)
+sideBarClose.addEventListener('click', closeNav)
+getPosts()
