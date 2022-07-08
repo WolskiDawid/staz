cc2793c21901768e373f18abb58d31aef8d16ba4
Dawid Wolski <dawidspore@wp.pl>
2022-07-08 15:46:10 +0200

    Pierwszy tydzien ostateczna wersja, sa bledy w skrypcie z walidacja (mozna ustawic m.in. wiek na 221 lat, ...), nie ma filtra jeszcze bo nie wiem jak sie to robi w axiosie

--- api.js
+++ api.js
@@ -1,14 +1,27 @@
 // GET
 import axios from 'axios';
 
-let divsDiv = document.querySelector('#divs')
-
 export function getPosts() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
     .then((res) => buildDivs(res.data))
     .catch((err) => console.log(err)) 
 }
-    
+
+let divsDiv = document.querySelector('#divs')
+let fId = document.querySelector('#fId')
+let fTitle = document.querySelector('#fTitle')
+let fBody = document.querySelector('#fBody')
+let fUserId = document.querySelector('#fUserId')    
+
+const buildDivs = (rows) => {
+    for(let i=0; i < rows.length; i++) {
+        let pDiv = document.createElement('div')
+        pDiv.setAttribute('id', 'apiP')
+        pDiv.innerHTML = `<div class='divUnder'>ID: ${rows[i].id}</div><div class='divUnder'>Tytuł: ${rows[i].title}</div><div class='divUnder'>Zawartość: ${rows[i].body}</div><div class='divUnder'>ID Użytkownika: ${rows[i].userId}</div>`
+        divsDiv.appendChild(pDiv)   
+    }
+}
+
 const openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";   
 }
@@ -16,18 +29,10 @@ const openNav = () => {
 const closeNav = () => {
     document.getElementById("mySidenav").style.width = "0";
 }
-const buildDivs = (rows) => {
-    for(let i=0; i < rows.length; i++) {
-        let pDiv = document.createElement('div')
-        pDiv.setAttribute('id', 'apiP')
-        pDiv.innerHTML = `<div class='divUnder'>Body: ${rows[i].body}</div><div class='divUnder'>ID: ${rows[i].id}</div><div class='divUnder'>Tytuł: ${rows[i].title}</div><div class='divUnder'>ID Użytkownika: ${rows[i].userId}</div>`
-        divsDiv.appendChild(pDiv)   
-    }
-}
 
 
-
-
+fId.addEventListener('input', buildDivs)
+fId.addEventListener('blur', buildDivs) 
 sideBarOpen.addEventListener('click', openNav)
 sideBarClose.addEventListener('click', closeNav)
 getPosts()
